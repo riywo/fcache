bd410dd3bebf282ee134d8f53abd30fcc1c4cddd
Ryosuke IWANAGA <riywo.jp@gmail.com>
2013-05-09 18:38:36 -0700

    fix example

--- example.sh
+++ example.sh
@@ -4,7 +4,7 @@ perl -e 'sleep 1; print "$$\n"' &
 perl -e 'sleep 1; print "$$\n"' &
 time wait
 
-./fcache 1 perl -e 'sleep 1; print "$$"' &
-./fcache 1 perl -e 'sleep 1; print "$$"' &
-./fcache 1 perl -e 'sleep 1; print "$$"' &
+./fcache 1 perl -e 'sleep 1; print "$$\n"' &
+./fcache 1 perl -e 'sleep 1; print "$$\n"' &
+./fcache 1 perl -e 'sleep 1; print "$$\n"' &
 time wait
